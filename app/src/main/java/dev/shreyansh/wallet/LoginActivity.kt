5fac29544b52717fdf8a89ffdea309393738ad44
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-01-14 18:42:23 +0530

    adds login flow

--- app/src/main/java/dev/shreyansh/wallet/LoginActivity.kt
+++ app/src/main/java/dev/shreyansh/wallet/LoginActivity.kt
@@ -1,18 +1,88 @@
 package dev.shreyansh.wallet
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import dev.shreyansh.wallet.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.loginButton.setOnClickListener {
+            signInFlow()
+        }
+    }
+
+    private fun signInFlow() {
+        val signInIntent = googleSignInClient.signInIntent
+        startForResult.launch(signInIntent)
+    }
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (task.isSuccessful) {
+                    val account: GoogleSignInAccount? = task.result
+                    if (account != null) {
+                        firebaseAuthWithGoogle(account)
+                    }
+                } else {
+                    Toast.makeText(this, "Error occurred : ${task.exception.toString()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Error occurred : ${it.exception.toString()}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/dev/shreyansh/wallet/MainActivity.kt
+++ app/src/main/java/dev/shreyansh/wallet/MainActivity.kt
@@ -1,12 +1,41 @@
 package dev.shreyansh.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import dev.shreyansh.wallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.logoutButton.setOnClickListener {
+            signOutFlow()
+        }
+
+    }
+
+    private fun signOutFlow() {
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googlesSignInClient = GoogleSignIn.getClient(this,gso)
+        googlesSignInClient.signOut()
+        FirebaseAuth.getInstance().signOut();
     }
 }
